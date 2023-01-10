013c83d839c825afe5c7e454c30e96929a2803a4
Joel D'Autilio <joeldautilio@gmail.com>
2023-01-09 21:00:32 -0300

    Place mark functionality

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const gameGrid = document.querySelector("#game-grid");
+const turnDisplay = document.querySelector("#turn-display");
 const X = "X";
 const O = "O";
-const S = " ";
+const N = "";
 
 const GameBoard = (() => {
   let boardArray = [
-    [X, O, X],
-    [O, X, S],
-    [X, S, X],
+    [N,N,N],
+    [N,N,N],
+    [N,N,N],
   ];
 
   const getBoardArray = () => {
@@ -15,24 +16,56 @@ const GameBoard = (() => {
     for (let i = 0; i < 3; i++) arrayCopy.push([...boardArray[i]]);
     return arrayCopy;
   };
-  return { getBoardArray };
+
+  const placeMark = (mark, x, y) => {
+    if (x < 0 || x > 2 || y < 0 || y > 2) return false;
+    if (boardArray[x][y] != N) return false;
+    if (mark != X && mark != O) return false;
+
+    boardArray[x][y] = mark;
+    return true;
+  };
+
+  return { getBoardArray, placeMark };
 })();
 
-const Player = () => {
-  return {};
+const Player = (name, mark) => {
+  const getMark = () => mark;
+  const getName = () => name;
+
+  return { getName, getMark };
 };
 
+let home = Player('joel', X);
+let away = Player('juan', O);
+
 const GameSession = (() => {
-  return {};
+  let turn = home;
+  turnDisplay.textContent = turn.getName();
+
+  const getTurn = () => turn;
+
+  const makeMove = (x, y) => {
+    let success = GameBoard.placeMark(turn.getMark(), x, y);
+    if (success) {
+      turn = turn == home ? away : home;
+      renderBoard(GameBoard.getBoardArray());
+      turnDisplay.textContent = turn.getName();
+    }
+  };
+
+  return { getTurn, makeMove };
 })();
 
 function renderBoard(boardArray) {
-  for (const row of boardArray) {
-    for (const element of row) {
-      let cell = document.createElement('div');
+  gameGrid.innerHTML = "";
+  for (const [i, row] of boardArray.entries()) {
+    for (const [j, element] of row.entries()) {
+      let cell = document.createElement("div");
       gameGrid.appendChild(cell);
-      cell.classList.add('game-grid-cell');
+      cell.classList.add("game-grid-cell");
       cell.textContent = element;
+      cell.addEventListener('click', () => GameSession.makeMove(i, j));
     }
   }
 }
